39b7b0ecf53e1dd2793df61384c176af212c9dd7
grantgambetta <35276011+grantgambetta@users.noreply.github.com>
2020-04-06 12:45:05 -0700

    Add files via upload

--- Covid-19/covid19_regression.R
+++ Covid-19/covid19_regression.R
@@ -1,20 +1,18 @@
-library(tidyverse)
+library(dplyr)
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 covid_data <- read.csv(url)
 head(covid_data)
 usa <- covid_data %>% 
-  filter(Country.Region == 'US')
-usa <- usa[, 5:length(usa)]
+  filter(Country.Region == 'US') %>%
+  select(48:length(covid_data))
 print(usa)
 sk <- covid_data %>% 
-  filter(Country.Region == 'Korea, South')
-sk <- sk[, 5:length(sk)]
+  filter(Country.Region == 'Korea, South') %>% 
+  select(48:length(covid_data))
 print(sk)
-us_values <- as.numeric(usa)
-us_values <- log(us_values[48:length(us_values)])
+us_values <- log(as.numeric(usa))
 print(us_values)
-sk_values <- as.numeric(sk)
-sk_values <- log(sk_values[48:length(sk_values)])
+sk_values <- log(as.numeric(sk))
 print(sk_values)
 x <- seq(length(us_values))
 print(x)
