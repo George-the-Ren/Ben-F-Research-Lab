3bd2b448c816c1a08bc6f71d8a6d44dc268ac7b4
grantgambetta <35276011+grantgambetta@users.noreply.github.com>
2020-04-23 13:49:11 -0700

    added more to plots

--- Covid-19/curve_fitting.R
+++ Covid-19/curve_fitting.R
@@ -4,16 +4,10 @@ library(ggplot2)
 # cases.raw <- 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
 deaths.raw <- 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 
-cases <- read.csv(cases.raw)
-head(cases)
-
 deaths <- read.csv(deaths.raw)
 head(deaths)
 
-cases <- cases %>% 
-  select(-Province.State, -Lat, -Long)
-print(cases)
-
+# DATA SUBSETTING
 deaths <- deaths %>% 
   select(-Province.State, -Lat, -Long)
 print(deaths)
@@ -39,112 +33,152 @@ colnames(subset)
 usa <- subset['usa']
 usa <- usa %>% 
   filter(usa > 0)
-print(usa)
 usa <- log(as.numeric(unlist(usa)))
 print(usa)
 
 x <- seq(length(usa))
 y <- usa
 
-model1 <- lm(y ~ poly(x, 2, raw=TRUE)) # second degree polynomial
-model2 <- lm(y ~ poly(x, 3, raw=TRUE)) # third degree polynomial
-model3 <- lm(y ~ poly(x, 4, raw=TRUE)) # fourth degree polynomial
-  
+model1 <- lm(y ~ poly(x, 2, raw=TRUE)) 
+model2 <- lm(y ~ poly(x, 3, raw=TRUE)) 
+model3 <- lm(y ~ poly(x, 4, raw=TRUE)) 
+
+summary(model1)
+summary(model2)
+summary(model3)
+
 ggplot(NULL, aes(x = x)) +
-  geom_point(aes(y = y)) +
-  geom_line(y = predict(model1, data.frame(x=x)), 
-            color = 'red') +
-  geom_line(y = predict(model2, data.frame(x=x)), 
-            color = 'blue') +
-  geom_line(y = predict(model3, data.frame(x=x)), 
-            color = 'green') +
+  geom_point(aes(y = y), size = 1.7) +
+  geom_line(aes(y = predict(model1, data.frame(x=x)), 
+                color = '2nd degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model2, data.frame(x=x)), 
+                color = '3rd degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model3, data.frame(x=x)), 
+                color = '4th degree'), 
+            size = 0.6) +
   labs(x = 'Days Since First Case', 
        y = 'Number of Cases (log)', 
        title = 'US COVID-19 Deaths') + 
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.9, 0.2),
+        legend.text = element_text(size=12),
+        legend.title = element_blank())
 
 # ITALY
 italy <- subset['italy']
 italy <- italy %>% 
   filter(italy > 0)
-print(italy)
 italy <- log(as.numeric(unlist(italy)))
 print(italy)
 
 x <- seq(length(italy))
 y <- italy
 
-model1 <- lm(y ~ poly(x, 2, raw=TRUE)) # second degree polynomial
-model2 <- lm(y ~ poly(x, 3, raw=TRUE)) # third degree polynomial
-model3 <- lm(y ~ poly(x, 4, raw=TRUE)) # fourth degree polynomial
+model1 <- lm(y ~ poly(x, 2, raw=TRUE)) 
+model2 <- lm(y ~ poly(x, 3, raw=TRUE)) 
+model3 <- lm(y ~ poly(x, 4, raw=TRUE)) 
+
+summary(model1)
+summary(model2)
+summary(model3)
 
 ggplot(NULL, aes(x = x)) +
-  geom_point(aes(y = y)) +
-  geom_line(y = predict(model1, data.frame(x=x)), 
-            color = 'red') +
-  geom_line(y = predict(model2, data.frame(x=x)), 
-            color = 'blue') +
-  geom_line(y = predict(model3, data.frame(x=x)), 
-            color = 'green') +
+  geom_point(aes(y = y), size = 1.7) +
+  geom_line(aes(y = predict(model1, data.frame(x=x)), 
+                color = '2nd degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model2, data.frame(x=x)), 
+                color = '3rd degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model3, data.frame(x=x)), 
+                color = '4th degree'), 
+            size = 0.6) +
   labs(x = 'Days Since First Case', 
        y = 'Number of Cases (log)', 
        title = 'Italy COVID-19 Deaths') + 
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.9, 0.2),
+        legend.text = element_text(size=12),
+        legend.title = element_blank())
 
 # SOUTH KOREA
 sk <- subset['south.korea']
 sk <- sk %>% 
   filter(sk > 0)
-print(sk)
 sk <- log(as.numeric(unlist(sk)))
 print(sk)
 
 x <- seq(length(sk))
 y <- sk
 
-model1 <- lm(y ~ poly(x, 2, raw=TRUE)) # second degree polynomial
-model2 <- lm(y ~ poly(x, 3, raw=TRUE)) # third degree polynomial
-model3 <- lm(y ~ poly(x, 4, raw=TRUE)) # fourth degree polynomial
+model1 <- lm(y ~ poly(x, 2, raw=TRUE)) 
+model2 <- lm(y ~ poly(x, 3, raw=TRUE)) 
+model3 <- lm(y ~ poly(x, 4, raw=TRUE)) 
+model4 <- lm(y ~ poly(x, 5, raw=TRUE)) 
+model5 <- lm(y ~ poly(x, 6, raw=TRUE)) 
+
+summary(model2)
+summary(model3)
+summary(model4)
+summary(model5)
 
 ggplot(NULL, aes(x = x)) +
-  geom_point(aes(y = y)) +
-  geom_line(y = predict(model1, data.frame(x=x)), 
-            color = 'red') +
-  geom_line(y = predict(model2, data.frame(x=x)), 
-            color = 'blue') +
-  geom_line(y = predict(model3, data.frame(x=x)), 
-            color = 'green') +
+  geom_point(aes(y = y), size = 1.7) +
+  geom_line(aes(y = predict(model2, data.frame(x=x)), 
+                color = '3rd degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model3, data.frame(x=x)), 
+                color = '4th degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model4, data.frame(x=x)), 
+                color = '5th degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model5, data.frame(x=x)), 
+                color = '6th degree'), 
+            size = 0.6) +
   labs(x = 'Days Since First Case', 
        y = 'Number of Cases (log)', 
        title = 'South Korea COVID-19 Deaths') + 
-  theme(plot.title = element_text(hjust = 0.5))
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.9, 0.2),
+        legend.text = element_text(size=12),
+        legend.title = element_blank())
 
 # SPAIN
 spain <- subset['spain']
 spain <- spain %>% 
   filter(spain > 0)
-print(spain)
 spain <- log(as.numeric(unlist(spain)))
 print(spain)
 
 x <- seq(length(spain))
 y <- spain
 
-model1 <- lm(y ~ poly(x, 2, raw=TRUE)) # second degree polynomial
-model2 <- lm(y ~ poly(x, 3, raw=TRUE)) # third degree polynomial
-model3 <- lm(y ~ poly(x, 4, raw=TRUE)) # fourth degree polynomial
+model1 <- lm(y ~ poly(x, 2, raw=TRUE)) 
+model2 <- lm(y ~ poly(x, 3, raw=TRUE)) 
+model3 <- lm(y ~ poly(x, 4, raw=TRUE)) 
+
+summary(model1)
+summary(model2)
+summary(model3)
 
 ggplot(NULL, aes(x = x), color = colors) +
-  geom_point(aes(y = y)) +
-  geom_line(y = predict(model1, data.frame(x=x)), 
-            color = 'red') +
-  geom_line(y = predict(model2, data.frame(x=x)), 
-            color = 'blue') +
-  geom_line(y = predict(model3, data.frame(x=x)), 
-            color = 'green') +
+  geom_point(aes(y = y), size = 1.7) +
+  geom_line(aes(y = predict(model1, data.frame(x=x)), 
+                color = '2nd degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model2, data.frame(x=x)), 
+                color = '3rd degree'), 
+            size = 0.6) +
+  geom_line(aes(y = predict(model3, data.frame(x=x)), 
+                color = '4th degree'), 
+            size = 0.6) +
   labs(x = 'Days Since First Case', 
        y = 'Number of Cases (log)', 
        title = 'Spain COVID-19 Deaths') + 
-  theme(plot.title = element_text(hjust = 0.5), 
-        legend.position=c(0.9, 0.9), 
-        legend.justification = c('right', 'bottom'))
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.9, 0.2),
+        legend.text = element_text(size=12),
+        legend.title = element_blank())
